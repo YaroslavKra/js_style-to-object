7b558464bc30697d6bab553c5bb53620d7ea1081
Yaroslav Kravchuk <Yaroslav.kravchyk@gmail.com>
2025-10-15 22:05:52 +0300

    Solution 3

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -18,7 +18,9 @@ function convertToObject(sourceString) {
       const key = line.slice(0, idx).trim();
       const value = line.slice(idx + 1).trim();
 
-      parsedStyles[key.trim()] = value.trim();
+      if (key && value) {
+        parsedStyles[key] = value;
+      }
     });
 
   return parsedStyles;
